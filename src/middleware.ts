95e11b77c4e06169a9e76a0d73cff0c594bfc77d
AhadDeveloper <ahad75366@gmail.com>
2025-12-24 19:56:11 +0500

    middleware error fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,17 @@
 import { type NextRequest } from "next/server";
 import { updateSession } from "./lib/supabase/middleware";
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+export async function proxy(request: NextRequest) {
+  try {
+    return await updateSession(request); // call your existing logic
+  } catch (err) {
+    console.error("Proxy error:", err);
+    return new Response("Internal Server Error", { status: 500 });
+  }
 }
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
